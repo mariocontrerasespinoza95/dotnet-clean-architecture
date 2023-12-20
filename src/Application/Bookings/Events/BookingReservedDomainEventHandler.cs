e4bfbaca72df2f98f8224de168eae7919b8ed2d9
Mario Contreras <marioce751@gmail.com>
2023-12-20 10:54:10 -0700

    Add an example domain event handler

---
+++ src/Application/Bookings/Events/BookingReservedDomainEventHandler.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Bookings.Events;
+using MediatR;
+
+namespace Application.Bookings.Events;
+internal sealed class BookingReservedDomainEventHandler : INotificationHandler<BookingReservedDomainEvent>
+{
+
+    public Task Handle(BookingReservedDomainEvent notification, CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+}
